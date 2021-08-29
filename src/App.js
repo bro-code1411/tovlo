9569ca62f661e6816006566cfb0d6c35e1cd739c
Lakshya Sharma <lakshyatu@gmail.com>
2021-08-29 23:05:48 +0530

    Added Google Analytics
    Added Google Analytics

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Error from "./pages/Error";
 import { Route, Switch } from "react-router-dom";
 import { Navbar } from "./componenets/Navbar";
 import TripDetails from "./pages/TripDetails";
+import ReactGA from "react-ga";
+import RouteChangeTracker from "./componenets/RouterChangeTracker";
+const TRACKING_ID = "G-J4LXKVS0BL";
+
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
     <>
+      <RouteChangeTracker />
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/trips" component={Trips} />
-        <Route exact path="/trips/:slug" component={TripDetails}/>
+        <Route exact path="/trips/:slug" component={TripDetails} />
         <Route component={Error} />
       </Switch>
     </>
