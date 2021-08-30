d08bed8905195435728c374b010a343ced04b023
Vartul Sharma <vartul14@gmail.com>
2021-08-30 22:17:04 +0530

    Search Page

--- src/componenets/TripList.js
+++ src/componenets/TripList.js
@@ -9,13 +9,32 @@ const TripList = ({ trips }) => {
       </div>
     );
   }
+  // return (
+  //   <section className="roomslist">
+  //     <div className="roomslist-center">
+  //       {trips.map((item) => {
+  //         return <Trip key={item.id} trip={item} />;
+  //       })}
+  //     </div>
+  //   </section>
+  // );
+
   return (
-    <section className="roomslist">
-      <div className="roomslist-center">
+    <section className="flex">
+      <section className="filter-container">
+        <p className="filter-para">500 Trips for 5 guest </p>
+        <h1 className="filter-heading">Trips in Kheerganga</h1>
+        <div className="filter-row">
+          <div className="filter-button">Cancellation Flexibility</div>
+          <div className="filter-button">Accomodation Type</div>
+          <div className="filter-button">Travel Type</div>
+          <div className="filter-button">Price</div>
+          <div className="filter-button">More filters</div>
+        </div>
         {trips.map((item) => {
           return <Trip key={item.id} trip={item} />;
         })}
-      </div>
+      </section>
     </section>
   );
 };
